0fba12fb03040beea5cfe868c540d7abbef36c6c
Chen Yufei <cyfdecyf@gmail.com>
2015-05-22 00:36:57 +0800

    Get 4 week before price from Yahoo.

--- 28.js
+++ 28.js
@@ -1,12 +1,12 @@
 // 沪深300 和中证500 指数
-var csi300_current = 0;
-var zz500_current = 0;
+var csi300Current = 0;
+var zz500Current = 0;
 
-var csi300_previous = 1;
-var zz500_previous = 1;
+var csi300Previous = 1;
+var zz500Previous = 1;
 
-var csi300_increase = 0;
-var zz500_increase = 0;
+var csi300Increase = 0;
+var zz500Increase = 0;
 
 // 计算 current 相比 previous 增长百分比
 function increasePercent(previous, current) {
@@ -20,47 +20,85 @@ function getCurrentQuote() {
 		async: false,
 		success: function (data) {
 			var arr = data.split('\n');
-			csi300_current = parseFloat(arr[0].split(',')[3]);
-			zz500_current = parseFloat(arr[1].split(',')[3]);
+			csi300Current = parseFloat(arr[0].split(',')[3]);
+			zz500Current = parseFloat(arr[1].split(',')[3]);
 		},
 		error: function(xhr, errType, error) {
-			alert("Failed to current stock quote.");
+			alert("Failed to get current index quote.");
 		}
 	});
 }
 
-// 获取指数过去价格
+function getQuoteFromYahoo(stock, callback) {
+	var today = new Date();
+	// 考虑到放假可能有一周时间不开盘，为确保能找到过去 4 周的收盘数据，从当前时间起往回 6 周
+	// Date 可以用 milisecond 构造，所以将当前时间减去 6 周对应的 milisecond 数
+	var startDate = new Date(today - 6*7*24*60*60*1000);
+	var baseUrl = 'http://real-chart.finance.yahoo.com/table.csv?s=';
+	var weeklyQuoteUrl = [
+		baseUrl, stock,
+		'&a=', startDate.getMonth(),
+		'&b=', startDate.getDate(),
+		'&c=', startDate.getFullYear(),
+		'&d=', today.getMonth(),
+		'&e=', today.getDate(),
+		'&f=', today.getFullYear(),
+		'&g=w&ignore=.csv'
+	].join('');
+
+	$.ajax({
+		url: weeklyQuoteUrl,
+		async: false,
+		success: function (data) {
+			// 获取 4 周前的收盘价
+			var weekQuote = data.split('\n')[4];
+			callback(parseFloat(weekQuote.split(',')[4]));
+		},
+		error: function(xhr, errType, error) {
+			alert("Failed to get previous index quote for " + stock);
+		}
+	});
+}
+
+// 获取指数往前 4 周时的收盘价（本周也算在内）
 function getPreviousQuote() {
+	getQuoteFromYahoo('000300.SS', function(price) {
+		csi300Previous = price;
+	});
+	// Yahoo 没有中证 500 指数的历史数据
+	getQuoteFromYahoo('000905.SS', function(price) {
+		zz500Previous = price;
+	});
 }
 
 function calculateIncrease() {
-	csi300_increase = increasePercent(csi300_previous, csi300_current);
-	zz500_increase = increasePercent(zz500_previous, zz500_current);
+	csi300Increase = increasePercent(csi300Previous, csi300Current);
+	zz500Increase = increasePercent(zz500Previous, zz500Current);
 }
 
 function updateUI() {
 	// 更新页面指数当前价格
-	$('#csi300_current').text(csi300_current.toFixed(2));
-	$('#zz500_current').text(zz500_current.toFixed(2));
+	$('#csi300Current').text(csi300Current.toFixed(2));
+	$('#zz500Current').text(zz500Current.toFixed(2));
 
 	// 更新页面指数过去价格
-	$('#csi300_previous').text(csi300_previous.toFixed(2));
-	$('#zz500_previous').text(zz500_previous.toFixed(2));
+	$('#csi300Previous').text(csi300Previous.toFixed(2));
+	$('#zz500Previous').text(zz500Previous.toFixed(2));
 
 	// 更新指数增幅
-	$('#csi300_increase').text(csi300_increase.toFixed(2) + '%');
-	$('#zz500_increase').text(zz500_increase.toFixed(2) + '%');
+	$('#csi300Increase').text(csi300Increase.toFixed(2) + '%');
+	$('#zz500Increase').text(zz500Increase.toFixed(2) + '%');
 
 	// 更新选择指数，高亮指数所在行
-	if (zz500_increase > csi300_increase) {
+	if (zz500Increase > csi300Increase) {
 		console.log("select zz500");
-		$('#zz500_tr').addClass('am-danger');
-		$('#csi300_tr').removeClass('am-danger');
+		$('#zz500Tr').addClass('am-danger');
+		$('#csi300Tr').removeClass('am-danger');
 		$('#next28').text('中证 500 指数');
 	} else {
 		console.log("select csi300");
-		$('#csi300_tr').addClass('am-danger');
-		$('#zz500_tr').removeClass('am-danger');
+		$('#csi300Tr').addClass('am-danger');
+		$('#zz500Tr').removeClass('am-danger');
 		$('#next28').text('沪深 300 指数');
 	}
 }
