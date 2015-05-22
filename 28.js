70948dc062bac1d96a289ac6c45856639f38749a
Chen Yufei <cyfdecyf@gmail.com>
2015-05-23 01:28:42 +0800

    Get previous quote from NetEase.

--- 28.js
+++ 28.js
@@ -32,30 +32,73 @@ function getCurrentQuote() {
 	});
 }
 
-function getQuoteFromYahoo(stock, callback) {
+// 比较最近 N 周的涨幅，则获取回退 N 周时的收盘价
+var LOOK_BACK_N_WEEK = 4;
+var WEEK_MILLISECOND = 7*24*60*60*1000;
+
+function parseDate(str) {
+	var parts = str.split('-');
+	return new Date(parts[0], parts[1] - 1, parts[2]); // Note: months are 0-based
+}
+
+function formatDate(d) {
+	var month = d.getMonth() + 1;
+	if (month < 10) {
+		month = [0, month].join('');
+	}
+	var date = d.getDate();
+	if (date < 10) {
+		date = [0, date].join('');
+	}
+	return [ d.getFullYear(), month, date ].join('');
+}
+
+function getQuoteFromNetEase(stock, callback) {
 	var today = new Date();
-	// 考虑到放假可能有一周时间不开盘，为确保能找到过去 4 周的收盘数据，从当前时间起往回 6 周
-	// Date 可以用 milisecond 构造，所以将当前时间减去 6 周对应的 milisecond 数
-	var startDate = new Date(today - 6*7*24*60*60*1000);
-	var baseUrl = 'http://real-chart.finance.yahoo.com/table.csv?s=';
-	var weeklyQuoteUrl = [
-		baseUrl, stock,
-		'&a=', startDate.getMonth(),
-		'&b=', startDate.getDate(),
-		'&c=', startDate.getFullYear(),
-		'&d=', today.getMonth(),
-		'&e=', today.getDate(),
-		'&f=', today.getFullYear(),
-		'&g=w&ignore=.csv'
+	// 放假可能有一周时间不开盘，为确保能找到过去 N 周的收盘数据，多取几周的数据
+	var startDate = new Date(today - (LOOK_BACK_N_WEEK + 2) * WEEK_MILLISECOND);
+
+	var dailyQuoteUrl = [
+		'http://quotes.money.163.com/service/chddata.html',
+		"?code=", stock,
+		'&start=', formatDate(startDate),
+		'&end=', formatDate(today),
+		'&fields=TCLOSE'
 	].join('');
 
+	// console.log(dailyQuoteUrl);
+
+	// 价格按日期逆序排列，一行一行数据往回走，倒退 1 周，返回上周最后一个交易日的索引
+	function goBack1Week(quote, start) {
+		var day2 = parseDate(quote[start].split(',')[0]);
+		for (var i = 1; i <= 5; i++) { // 一周最多 5 个交易日
+			var day1 = parseDate(quote[start + i].split(',')[0]);
+			// 往回走一天，若星期几变大，说明一定倒退了一周
+			// 若天数差异 >= 7，也一定回退了一周（休市一周时星期几不会回退）
+			if ((day1.getDay() >= day2.getDay()) ||
+				(day2 - day1 >= WEEK_MILLISECOND)) {
+				return start + i;
+			}
+			day2 = day1;
+		}
+	}
+
+	function getClosePrice(data) {
+		var quote = data.split('\n');
+		var idx = 1; // 跳过第一行列说明
+		// 如需比较最近 4 周的涨幅，回退 4 周的收盘价和本周的收盘价进行比较
+		for (var i = 0; i < LOOK_BACK_N_WEEK; i++) {
+			idx = goBack1Week(quote, idx);
+			console.log("back", i, quote[idx].split(',')[0]);
+		}
+		return parseFloat(quote[idx].split(',')[3]);
+	}
+
 	$.ajax({
-		url: weeklyQuoteUrl,
+		url: dailyQuoteUrl,
 		async: false,
 		success: function (data) {
-			// 获取 4 周前的收盘价
-			var weekQuote = data.split('\n')[4];
-			callback(parseFloat(weekQuote.split(',')[4]));
+			callback(getClosePrice(data));
 		},
 		error: function(xhr, errType, error) {
 			alert("Failed to get previous index quote for " + stock);
@@ -65,12 +108,11 @@ function getQuoteFromYahoo(stock, callback) {
 
 // 获取指数往前 4 周时的收盘价（本周也算在内）
 function getPreviousQuote() {
-	getQuoteFromYahoo('000300.SS', function(price) {
-		csi300.previous = price;
+	getQuoteFromNetEase('0000300', function(quote) {
+		csi300.previous = quote;
 	});
-	// Yahoo 没有中证 500 指数的历史数据，所以不能使用了
-	getQuoteFromYahoo('000905.SS', function(price) {
-		zz500.previous = price;
+	getQuoteFromNetEase('0000905', function(quote) {
+		zz500.previous = quote;
 	});
 }
 
